7a196371e3b3b988776da3c5c00922799d70ea0e
raed elmajdoub <raed@eng.leadshook.com>
2023-01-11 20:34:58 +0100

    create first image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-   <div>
-      <p>abir elmajdoub</p>
-   </div>
+   <>
+  <div className='container-image'>
+    <img src='https://powerpackelements.com/wp-content/uploads/2018/01/travel-row-1.jpg' alt=''></img>
+    <p class="centered text-pr">Amazing Santorini 7 Days Tour</p>
+    <p class="centered text-pr2">A modern & beautiful WordPress theme for all travel & tourism business.</p>
+    <button className='centered button'>book now</button>
+  </div>
+  <div className='container'>
+    <div className='row shadow-lg p-3 mb-5 bg-body rounded mt-top'>
+      <div className='col-xl-3 col-6'>
+      <div className='row'>
+        <div className='col-4'>
+        <img width="50" height="52" src="https://powerpackelements.com/wp-content/uploads/2018/01/travel-i3.png"  alt="" loading="lazy" />
+        </div>
+        <div className='col-4'>
+        <p>345</p>
+        <p>Tours</p>
+        </div>
+      </div>
+      </div>
+      <div className='col-xl-3 col-6'>
+      <div className='row'>
+        <div className='col-4'>
+        <img width="50" height="52" src="https://powerpackelements.com/wp-content/uploads/2018/01/travel-i4.png" alt="" loading="lazy" />
+        </div>
+        <div className='col-4'>
+        <p>345</p>
+        <p>Tours</p>
+        </div>
+      </div>
+      </div>
+      <div className='col-xl-3 col-6'>
+      <div className='row'>
+        <div className='col-4'>
+        <img width="50" height="52" src="https://powerpackelements.com/wp-content/uploads/2018/01/travel-i2.png" alt="" loading="lazy" />
+        </div>
+        <div className='col-4'>
+        <p>345</p>
+        <p>Tours</p>
+        </div>
+      </div>
+      </div>
+      <div className='col-xl-3 col-6'>
+      <div className='row'>
+        <div className='col-4'>
+        <img width="50" height="52" src="https://powerpackelements.com/wp-content/uploads/2018/01/travel-i1.png" alt="" loading="lazy" />
+        </div>
+        <div className='col-4'>
+        <p>345</p>
+        <p>Tours</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+   </>
   );
 }
 
