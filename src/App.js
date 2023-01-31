d5a19b92b73702db39600c177fe6cafdf6f868b2
abirelmajdoub <elmajdoub.abir@gmail.com>
2023-01-31 20:45:47 +0100

    last changes about changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 function App() {
   return (
    <>
+   
   <div className='container-image'>
-    <img src='https://powerpackelements.com/wp-content/uploads/2018/01/travel-row-1.jpg' alt=''></img>
-    <p class="centered text-pr">Amazing Santorini 7 Days Tour</p>
-    <p class="centered text-pr2">A modern & beautiful WordPress theme for all travel & tourism business.</p>
+    <img className='first-img' src='https://powerpackelements.com/wp-content/uploads/2018/01/travel-row-1.jpg' alt=''></img>
+    <p className="centered text-pr">Amazing Santorini 7 Days Tour</p>
+    <p className="centered text-pr2">A modern & beautiful WordPress theme for all travel & tourism business.</p>
     <button className='centered button'>book now</button>
   </div>
   <div className='container'>
@@ -61,26 +62,102 @@ function App() {
       <p className='text-center text-muted'>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 sed do eiusmod tempor incididunt</p>
     </div>
-    <div className='mt-5'>
+    <div className='mt-5 container-image'>
       <div className='row'>
-        <div className='col-4'>
-          <img  className='w-100 rounded' src='https://guidevoyage.org/wp-content/uploads/2011/02/la-tour-eiffel.jpg' height={350} />
+      <div className='col-4'>
+        <div className='col-12 img-col' style={{backgroundImage: "url('https://guidevoyage.org/wp-content/uploads/2011/02/la-tour-eiffel.jpg')"}}>
+          <button className='button-b'>%20</button>
         </div>
-        <div className='col-4'>
-          <img className='w-100 rounded' src='https://www.remax.eu/uploads/agent-1/Greece%20(Optimized)-5e580fa45d9f1.jpg' height={350} />
         </div>
+
         <div className='col-4'>
-          <img className='w-100 rounded' src='https://www.state.gov/wp-content/uploads/2018/11/Spain-2109x1406.jpg'  height={350}/>
+        <div className='col-12 img-col' style={{backgroundImage: "url('https://handluggageonly.co.uk/wp-content/uploads/2015/05/Hand-Luggage-Only-7.jpg')"}}>
+          <button className='button-b'>%20</button>
         </div>
+        </div>
+
+
+        <div className='col-4'>
+        <div className='col-12 img-col' style={{backgroundImage: "url('https://www.state.gov/wp-content/uploads/2018/11/Spain-2109x1406.jpg')"}}>
+          <button className='button-b'>%20</button>
+        </div>
+        </div>
+
         <div className='mt-5 col-8'>
-          <img className='w-100 rounded' src='https://media.cntraveler.com/photos/59404f9157d01262a807f8c9/16:9/w_5087,h_2861,c_limit/GettyImages-175386102.jpg' height={350}/>
+        <div className='col-12 img-col' style={{backgroundImage: "url('https://media.cntraveler.com/photos/59404f9157d01262a807f8c9/16:9/w_5087,h_2861,c_limit/GettyImages-175386102.jpg')"}}>
+          <button className='button-b'>%20</button>
+        </div>     
         </div>       
          <div className='mt-5 col-4'>
-          <img className='w-100 rounded' src='https://media.architecturaldigest.com/photos/5903ad5c1732ef7d3c2b9e38/4:3/w_2663,h_1997,c_limit/5%20Seaside%20Towns%20in%20Italy.jpg' height={350}/>
+          <div className='col-12 img-col' style={{backgroundImage: "url('https://media.architecturaldigest.com/photos/5903ad5c1732ef7d3c2b9e38/4:3/w_2663,h_1997,c_limit/5%20Seaside%20Towns%20in%20Italy.jpg')"}}>
+            <button className='button-b'>%20</button>
+          </div>             
         </div>
       </div>
     </div>
   </div>
+   <div className='img-bg mt-5'>
+    <div className='space-top'>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <h1 className="text-center  ">Travel far enough, you meet yourself</h1>
+      <p className="text-center text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button className='button-center button'>book now</button>
+    </div>
+    <div className='mar'>
+      <h1 className='text-center'> Most Popular Travel Countries</h1>
+      <p className='text-center text-muted'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div className='mt-5 container'>
+      <div className='row'>
+        <div className='col-6 container-image'>
+          <div className='col-12 img-col' style={{backgroundImage: "url('https://www.state.gov/wp-content/uploads/2018/11/Spain-2109x1406.jpg')"}}>
+            <button className='button-b'>%20</button>
+          </div>
+        </div>
+        <div className='col-6 container-image'>
+          <div className='col-12 img-col' style={{backgroundImage: "url('https://photo620x400.mnstatic.com/49b83f63ecebb9796010a8f345652297/plaza-del-callao.jpg')"}}>
+              <button className='button-b'>%20</button>
+            </div>     
+        </div>
+        <div className='mt-5 col-6 container-image'>
+          <div className='col-12 img-col' style={{backgroundImage: "url('https://media.nomadicmatt.com/2021/taipeithings1a.jpg')"}}>
+                <button className='button-b'>%20</button>
+          </div> 
+        </div>
+        <div className='mt-5 col-6 container-image'>
+          <div className='col-12 img-col' style={{backgroundImage: "url('https://mapio.net/images-p/71080800.jpg')"}}>
+            <button className='button-b'>%20</button>
+          </div> 
+        </div>       
+      </div>
+    </div>
+
+    <div className='mt-5 mb-3'>
+      <button className='know mb-3 button-center'>know more</button>
+    </div>
+    <div className='container'>
+    <div className='mar hr'></div>
+    
+      <div className='mt-5 call'>
+      <h1 className="book"> calling for booking</h1>
+      +1-800-123-4567
+      </div>
+    </div>
+    <div className='row mt-5 text'>
+      <div className='col-6'>
+        <h3>Lorem ipsum dolor</h3>
+        <p>Lorem ipsum dolor sit amet,</p>  
+        <p>consectetur adipisicing elit, sed do</p>
+        <p>eiusmod tempor incididunt.</p>
+      </div>
+      <div className='col-6'>
+        <h3>Contact Us</h3>
+        <p>1-800-123-4567</p>
+        <p>108 Thomaston Road, Preston CT</p>
+        <p>info@example.com</p>
+      </div>
+    </div>
+   </div>
    </>
   );
 }
